0695b97e07863ff7add0172f6928ba90fa3e394f
Joshua.C.Lin <joshuaclin77@gmail.com>
2021-05-07 14:24:12 +0800

    1st ver push

--- startbootstrap-shop-item-gh-pages/passlist.js
+++ startbootstrap-shop-item-gh-pages/passlist.js
@@ -1,204 +1,252 @@
 $(function () {
-    $('#select_data').on('change', function () {
-        searchClickOption($("#select_data").val());
-    })
-
+    // $('#select_data').on('change', function () {
+    //     searchClickOption($("#select_data").val());
+    // })
+  
     // $('#select_station').on('change', function () {
     //     searchClickOption($("#select_station").val());
     // })
-
+  
     // $('#id_btn_add').on('click', function() {
     //     console.log( "The ADD() function result is", ADD( $('#aba').val(), $('#cdc').val() ) ) ;
     // }) ;
     // $('#id_btn_adda').on('click', function() {
     //     console.log( "The ADD() function result is", ADD( $('#abaa').val(), $('#cdca').val() ) ) ;
     // }) ;
-
-
+  
     Date.prototype.format = function (format) {
-        //eg:format="yyyy-MM-dd hh:mm:ss";
-
-
-        if (!format) {
-            format = "yyyy-MM-dd hh:mm:ss";
-            // var date = { "M+": this.getMonth() + 1, "d+": this.getDate(), "h+": this.getHours(), "m+": this.getMinutes(), "s+": this.getSeconds(), "q+": Math.floor((this.getMonth() + 3) / 3), "S+": this.getMilliseconds() }; if (/(y+)/i.test(format)) { format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length)); } for (var k in date) { if (new RegExp("(" + k + ")").test(format)) { format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length)); } } return format; 
+      //eg:format="yyyy-MM-dd hh:mm:ss";
+  
+      if (!format) {
+        format = "yyyy-MM-dd hh:mm:ss";
+        // var date = { "M+": this.getMonth() + 1, "d+": this.getDate(), "h+": this.getHours(), "m+": this.getMinutes(), "s+": this.getSeconds(), "q+": Math.floor((this.getMonth() + 3) / 3), "S+": this.getMilliseconds() }; if (/(y+)/i.test(format)) { format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length)); } for (var k in date) { if (new RegExp("(" + k + ")").test(format)) { format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length)); } } return format;
+      }
+  
+      // Default today's date --- START
+      // var date = new Date();
+  
+      // var day = date.getDate();
+      // var month = date.getMonth() + 1;
+      // var year = date.getFullYear();
+  
+      // if (month < 10) month = "0" + month;
+      // if (day < 10) day = "0" + day;
+  
+      // var today = year + "-" + month + "-" + day;
+      // $("#select_data").attr("value", today);
+      // Default today's date ---
+  
+      var o = {
+        "M+": this.getMonth() + 1, // month
+        "d+": this.getDate(), // day
+        "H+": this.getHours(), // hour
+        "h+": this.getHours(), // hour
+        "m+": this.getMinutes(), // minute
+        "s+": this.getSeconds(), // second
+        "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
+        S: this.getMilliseconds(),
+      };
+  
+      if (/(y+)/.test(format)) {
+        format = format.replace(
+          RegExp.$1,
+          (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+        );
+      }
+  
+      for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+          format = format.replace(
+            RegExp.$1,
+            RegExp.$1.length == 1
+              ? o[k]
+              : ("00" + o[k]).substr(("" + o[k]).length)
+          );
         }
-
-        // Default today's date --- START
-        // var date = new Date();
-
-        // var day = date.getDate();
-        // var month = date.getMonth() + 1;
-        // var year = date.getFullYear();
-
-        // if (month < 10) month = "0" + month;
-        // if (day < 10) day = "0" + day;
-
-        // var today = year + "-" + month + "-" + day;       
-        // $("#select_data").attr("value", today);
-        // Default today's date --- 
-
-        var o = {
-            "M+": this.getMonth() + 1,  // month
-            "d+": this.getDate(),       // day
-            "H+": this.getHours(),      // hour
-            "h+": this.getHours(),      // hour
-            "m+": this.getMinutes(),    // minute
-            "s+": this.getSeconds(),    // second
-            "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
-            "S": this.getMilliseconds()
-        };
-
-
-        if (/(y+)/.test(format)) {
-            format = format.replace(RegExp.$1, (this.getFullYear() + "")
-                .substr(4 - RegExp.$1.length));
-        }
-
-        for (var k in o) {
-            if (new RegExp("(" + k + ")").test(format)) {
-                format = format.replace(RegExp.$1, RegExp.$1.length == 1
-                    ? o[k]
-                    : ("00" + o[k]).substr(("" + o[k]).length));
-            }
-        }
-
-        return format;
+      }
+  
+      return format;
     };
-
-});
-function sendList() {
-    let dt = new Date().format("yyyyMMddHHmmss");
-    let SHA1 = hex_sha1(
-        "FUNC_ID=QueryManifest&SYS_DATE=" + dt
-        + "&ACNT_NO=" + "088325806"
-        + "&API_KEY=" + "31823206");
-
-    console.log('SHA:', SHA1);
-    console.log('dt:', dt);
-
-    let dataJson = JSON.stringify({
-        "FUNC_ID": "QueryManifest",
-        "SYS_DATE": "20200101141030",
-        "ACNT_NO": "088325806",
-        "FUNC_SIG": "f6531a017fe8b4de8d46ecd3becc9e23ddcb53b2",
-        "FUNC_DATA": {
-            "SHIPPING_DATE": "2021-04-20",
-            // apiGetDate
-            "STATION": "D",
-            "VOYAGE": "DL357-1236",
-            "VOYAGE_TIME": "0800"
-        }
-    })
-
-    let passInfo_OP = $.ajax({
-        url: "https://hanggangjiu.tungliu.tw/API",
-        method: 'POST',
-        data: dataJson,
-        success: function (res) {
-            console.log(res);
-            ListData(res.RSPN_DATA);
-        }
-    })
-}
-function ListData(data) {
-
-    let table_html = '';
-    let table_info = '<tr><td>' + data.SHPIPING_DATE + '</td><td>' + data.VESSEL_ID + '</td><td>' + data.VESSEL_NAME + '</td><td>' + data.STATION + '</td><td>' + data.VOYAGE + '</td><td>' + data.VOYAGE_TIME + '</td><td>' + data.PASSENGER_CNT + '</td>'
-
-    let table_infoMation = '';
-    for (let index = 0; index < data.MANIFEST.length; index++) {
-
-        table_infoMation += table_info + '<td>' + data.MANIFEST[index].TICKET_NO + '</td><td>' + data.MANIFEST[index].BOARDING_DT + '</td><td>' + data.MANIFEST[index].ID_TYPE + '</td><td>' + data.MANIFEST[index].ID_NO + '</td><td>' + data.MANIFEST[index].NAME + '</td></tr>'
-    }
-    $('#table_data').html(table_html);
-    $('#table_data').html(table_info);
-    $('#table_data').html(table_infoMation);
-}
-
-function searchClickOption(apiGetDate) {
-    let dt = new Date().format("yyyyMMddHHmmss");
-    let SHA1 = hex_sha1(
-        "FUNC_ID=QueryManifest&SYS_DATE=" + dt
-        + "&ACNT_NO=" + "088325806"
-        + "&API_KEY=" + "31823206");
-
-    console.log('SHA:', SHA1);
-    console.log('dt:', dt);
-
-    let dataJson = JSON.stringify({
-        "FUNC_ID": "QueryVessel",
-        "SYS_DATE": "20200101141030",
-        "ACNT_NO": "088325806",
-        "FUNC_SIG": "f6531a017fe8b4de8d46ecd3becc9e23ddcb53b2",
-        "FUNC_DATA": {
-            "SHIPPING_DATE": apiGetDate,
-        }
-    })
-
-    let passInfo_OP = $.ajax({
-        url: "https://hanggangjiu.tungliu.tw/API",
-        method: 'POST',
-        data: dataJson,
-        success: function (res) {
-            console.log(res);
-            SelectData(res.RSPN_DATA);
-        }
-    })
-    function SelectData(data) {
-
-        let table_html = '';
-        let table_info = '<tr><td>' + data.SHPIPING_DATE + '</td><td>' + data.VESSEL_ID + '</td><td>' + data.VESSEL_NAME + '</td><td>' + data.STATION + '</td><td>' + data.VOYAGE + '</td><td>' + data.VOYAGE_TIME + '</td><td>' + data.PASSENGER_CNT + '</td>'
-
-        let table_infoMation = '';
-        for (let index = 0; index < data.MANIFEST.length; index++) {
-
-            table_infoMation += table_info + '<td>' + data.MANIFEST[index].TICKET_NO + '</td><td>' + data.MANIFEST[index].BOARDING_DT + '</td><td>' + data.MANIFEST[index].ID_TYPE + '</td><td>' + data.MANIFEST[index].ID_NO + '</td><td>' + data.MANIFEST[index].NAME + '</td></tr>'
-        }
-        $('#table_data').html(table_html);
-        $('#table_data').html(table_info);
-        $('#table_data').html(table_infoMation);
-    }
-
-}
-
-
-function SelectData(data) {
-    let select_data = '<input>' + data.SHIPPING_DATE + '</input>'
-    console.log(select_data)
-}
-function checkSelectCname() {
-    $.ajax({
-        type: "POST",
-        url: 'https://hanggangjiu.tungliu.tw/APIp',
-        dataType: "json",
-        success: function (result) {
-            document.getElementById(res.RSPN_DATA.VESSELS.VOYAGE_TIME).innerHTML = result;
-        }
+  
+    $("#select_data").on("change", function () {
+      onDateSelected($("#select_data").val());
     });
-}
-
-function apiStation(data) {
-
-    let table_html = '';
-    let table_info = '<option>' + data.STATION + '</option>'
-
-    let table_infoMation = '';
-    for (let index = 0; index < data.VESSELS.length; index++) {
-        table_infoMation += table_info + '<option>' + data.STATION.D + '</option><option>' + data.STATION.D + '</option>'
-    }
-    $('#select_timeTable').html(table_html);
-    $('#select_timeTable').html(table_info);
-    $('#select_timeTable').html(table_infoMation);
-}
-
-
-
-
-    // function ADD(number1, number2){
-    //     console.log( "number1 is ", number1 ) ;
-    //     console.log( "number2 is ", number2 ) ;
-    //     return parseInt( number1 ) + parseInt( number2 );
-    // }
-
-
+  
+    let initalDate = new Date().format("yyyy-MM-dd");
+    onDateSelected(initalDate);
+    $("#select_data").val(initalDate);
+  });
+  
+  function onDateSelected(dateString) {
+    let dt = new Date().format("yyyyMMddHHmmss");
+    let SHA1 = hex_sha1(
+      "FUNC_ID=QueryVessel&SYS_DATE=" +
+        dt +
+        "&ACNT_NO=" +
+        "088325806" +
+        "&API_KEY=" +
+        "31823206"
+    );
+  
+    let vesselData = JSON.stringify({
+      FUNC_ID: "QueryVessel",
+      SYS_DATE: "20200101141030",
+      ACNT_NO: "088325806",
+      FUNC_SIG: SHA1,
+      FUNC_DATA: {
+        SHIPPING_DATE: dateString,
+      },
+    });
+  
+    // let manifestData = JSON.stringify({
+    //     "FUNC_ID": "QueryManifest",
+    //     "SYS_DATE": "20200101141030",
+    //     "ACNT_NO": "088325806",
+    //     "FUNC_SIG": "f6531a017fe8b4de8d46ecd3becc9e23ddcb53b2",
+    //     "FUNC_DATA": {
+    //         "SHIPPING_DATE": "2021-04-20",
+    //         // apiGetDate
+    //         "STATION": "D",
+    //         "VOYAGE": "DL357-1236",
+    //         "VOYAGE_TIME": "0800"
+    //     }
+    // })
+  
+    $.ajax({
+      url: "https://hanggangjiu.tungliu.tw/API",
+      method: "POST",
+      data: vesselData,
+      success: function (res) {
+        console.log(res);
+        // SelectData(res.RSPN_DATA);
+        showVoyageOptions(res.RSPN_DATA);
+      },
+    });
+  }
+  
+  function showVoyageOptions(response) {
+    let options = new Set(response.VESSELS.map((vessel) => vessel.STATION));
+    // console.log(options)
+    let optionsArray = Array.from(options);
+    // console.log(optionsArray)
+    console.log(response.SHPIPING_DATE);
+    $("#select_station").html(
+      optionsArray
+        .map(
+          (option) =>
+            `<option value=${option}>${
+              option === "D" ? "東港" : "小琉球"
+            }</option>`
+        )
+        .join("")
+    );
+  
+    renderTimeOptionsByVoyage(
+      response.VESSELS,
+      $("#select_station").val(),
+      response.SHPIPING_DATE
+    );
+    $("#select_station").on("change", (e) => {
+      renderTimeOptionsByVoyage(
+        response.VESSELS,
+        $("#select_station").val(),
+        response.SHPIPING_DATE
+      );
+    });
+  }
+  
+  function renderTimeOptionsByVoyage(vessels, voyage, DATE) {
+    let options = vessels
+      .filter((vessel) => vessel.STATION === voyage)
+      .map((vessel) => vessel.VOYAGE)
+      .sort();
+    console.log(DATE);
+    $("#select_timeTable").html(
+      options
+        .map(
+          (option) =>
+            `<option value=${option}>${
+              option.slice(0, 2) + ":" + option.slice(2, 4)
+            }</option>`
+        )
+        .join("")
+    );
+  
+    let filteredVessels = vessels.filter(
+      (vessel) =>
+        vessel.STATION === voyage &&
+        vessel.VOYAGE === $("#select_timeTable").val()
+    );
+    console.log(filteredVessels);
+    renderInfoBoard(filteredVessels, DATE);
+    $("#select_timeTable").on("change", () => {
+      let filteredVessels = vessels.filter(
+        (vessel) =>
+          vessel.STATION === voyage &&
+          vessel.VOYAGE === $("#select_timeTable").val()
+      );
+      renderInfoBoard(filteredVessels, DATE);
+    });
+  }
+  
+  function renderInfoBoard(vessels, DATE) {
+      $("#table_data").empty();
+    // api2
+    let dt = new Date().format("yyyyMMddHHmmss");
+    let SHA2 = hex_sha1(
+      "FUNC_ID=QueryManifest&SYS_DATE=" +
+        dt +
+        "&ACNT_NO=" +
+        "088325806" +
+        "&API_KEY=" +
+        "31823206"
+    );
+  
+    vessels.forEach( (vessel) => {
+      let manifestData = JSON.stringify({
+        FUNC_ID: "QueryManifest",
+        SYS_DATE: dt,
+        ACNT_NO: "088325806",
+        FUNC_SIG: SHA2,
+        FUNC_DATA: {
+          SHIPPING_DATE: DATE,
+          STATION: vessel.STATION,
+          VOYAGE: vessel.VOYAGE,
+          VOYAGE_TIME: vessel.VOYAGE_TIME,
+        },
+      });
+  
+      $.ajax({
+        url: "https://hanggangjiu.tungliu.tw/API",
+        method: "POST",
+        data: manifestData,
+        success: function (res) {
+          console.log(res);
+          console.log(vessel)
+          let manifest = res.RSPN_DATA.MANIFEST;
+          let info = manifest.map(mani=>{
+              return `
+              <tr>
+                <td>${DATE}</td>
+                <td>${vessel.VESSEL_ID}</td>
+                <td>${vessel.VESSEL_NAME}</td>
+                <td>${vessel.STATION}</td>
+                <td>${vessel.VOYAGE}</td>
+                <td>${vessel.VOYAGE_TIME}</td>
+                <td>${vessel.PASSENGER_NO}</td>
+                <td>${mani.TICKET_NO}</td>
+                <td>${mani.BOARDING_DT}</td>
+                <td>${mani.ID_TYPE}</td>
+                <td>${mani.ID_NO}</td>
+                <td>${mani.NAME}</td>
+              </tr>
+              `
+          })
+  
+          $("#table_data").append(info);
+  
+        },
+      });
+    });
+  }
+  
